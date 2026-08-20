a4c6aef77f7baafe9b835a8e15d983faa61a32a3
phyrozz <joshuamalabanan70@gmail.com>
2026-08-21 05:26:16 +0800

    fixed images not loading

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from 'next'
 const nextConfig: NextConfig = {
   allowedDevOrigins: ['192.168.18.7'],
   images: {
+    unoptimized: true,
     remotePatterns: [
       {
         protocol: 'https',
